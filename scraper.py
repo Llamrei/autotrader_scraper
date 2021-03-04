29d0783e6ed79946103e9b0d038cbafeaad56ef9
Llamrei <alex@larionov.co.uk>
2021-03-04 19:46:14 +0000

    Updated scraper to play nicely
    Also backup now works and prints small status message

--- scraper.py
+++ scraper.py
@@ -2,19 +2,38 @@ import logging
 import pickle as pkl
 import re
 import sys
-from datetime import timedelta
+from logging.handlers import RotatingFileHandler
 from pathlib import Path
 from time import sleep
+from types import new_class
 from urllib.parse import parse_qs, urljoin, urlparse
 
 import cfscrape
 from bs4 import BeautifulSoup as bs
 
-logging.basicConfig(filename='scraper.log', encoding='utf-8', filemode='w', level=logging.DEBUG)
+rfh = logging.handlers.RotatingFileHandler(
+    filename='data/scraper.log', 
+    mode='w',
+    maxBytes=1*1024*1024*1024,
+    backupCount=2,
+    encoding=None,
+    delay=0
+)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(name)-25s %(levelname)-8s %(message)s",
+    datefmt="%y-%m-%d %H:%M:%S",
+    handlers=[
+        rfh
+    ]
+)
+
+# Deeply nested data structure means recursive pickling needs a larger stack to work with
+sys.setrecursionlimit(50000)
 
 # Check for previous run
 if Path('page_reached.tmp').is_file():
-    logging.debug('Detected previous attempted run - restarting on: ', end='')
+    logging.debug('Detected previous attempted run - restarting on: ')
     with open('page_reached.tmp', 'r') as f:
         INITIAL_PAGE = int(f.readline())
     logging.debug(f"page {INITIAL_PAGE}")
@@ -22,45 +41,47 @@ else:
     INITIAL_PAGE = 1
     logging.debug(f'No previous run - starting from page {INITIAL_PAGE}')
 
-OUTPUT_FILE = sys.argv[1]
-BACKUP_FREQ = 50      # In pages
+BACKUP_FREQ = 10     # pages
 SORT_KEY = 'relevance'
 GUID_PATTERN = 'window\.AT\.correlationId = "([\w|.|-]+)'
-DELAY = 5 # seconds
+DELAY = 1 # seconds
 TIMEOUT = 3.05 # seconds
 
-root = 'https://www.autotrader.co.uk'
-json_endpoint = urljoin(root, '/json/fpa/initial/')
-details_endpoint = urljoin(root, '/car-details/')
-search = urljoin(root, '/car-search')
-tech_specs = urljoin(root,'/json/taxonomy/technical-specification')
+ROOT = 'https://www.autotrader.co.uk'
+JSON_ENDPOINT = urljoin(ROOT, '/json/fpa/initial/')
+DETAILS_ENDPOINT = urljoin(ROOT, '/car-details/')
+SEARCH_ENDPOINT = urljoin(ROOT, '/car-search')
+SPECS_ENDPOINT = urljoin(ROOT,'/json/taxonomy/technical-specification')
 
 data = []
-scrape_count = 0
+page = INITIAL_PAGE
+backup_page = INITIAL_PAGE
+
 # TODO: Look into using a session that stores cookies?
 scraper = cfscrape.create_scraper()
 
-page = INITIAL_PAGE
-backup_page = INITIAL_PAGE
+# Get first search of all ads - do outside loop to retrieve how many pages we can scrape
 params = {'postcode':'eh42ar', 'page': page, 'sort':SORT_KEY}
-search_page = scraper.get(search, params=params)
+search_page = scraper.get(SEARCH_ENDPOINT, params=params)
 soup = bs(search_page.content, 'html.parser')
-max_page = soup.find('li', class_='paginationMini__count').contents[1].string
+max_page = int(soup.find('li', class_='paginationMini__count').contents[3].string)
 delay_s = search_page.elapsed.total_seconds()
 logging.debug(f'Starting scrape on page {page}/{max_page}')
 
 while page < max_page:
     ads = soup.body.main.find_all('li', class_='search-page__result')
-
+    ad_idx = 1
     for ad in ads:
-        sleep(max(delay_s,1))
+        sleep(max(delay_s,DELAY))
+        status_string = f"\r Page {page}/{max_page} | Ad {ad_idx}/{len(ads)}                      "
+        print(status_string, end='')
         images = []
         # Get ad page and price
         ad_url = ad.find('a', class_='js-click-handler').attrs['href']
         price = ad.find('div', class_="product-card-pricing__price").find('span').string
         price_float = float(price.strip('£').replace(',',''))
         ad_id = ad.attrs['id']
-        ad_page = scraper.get(urljoin(root, ad_url), timeout=TIMEOUT)
+        ad_page = scraper.get(urljoin(ROOT, ad_url), timeout=TIMEOUT)
 
         # Get correct params from ad page for API to accept the request to JSON endpoint
         ad_guid = re.search(GUID_PATTERN, str(ad_page.content)).group(1)
@@ -68,32 +89,46 @@ while page < max_page:
         params = parse_qs(details_obj.query)
 
         # Get raw data used to fill in the ad page from JSON endpoint
-        ad_details = scraper.get(urljoin(json_endpoint, ad_id), params = {**params, 'guid':ad_guid}, timeout=TIMEOUT)
+        ad_details = scraper.get(urljoin(JSON_ENDPOINT, ad_id), params = {**params, 'guid':ad_guid}, timeout=TIMEOUT)
         ad_json = ad_details.json()
         image_urls = ad_json['advert']['imageUrls']
         for image_url in image_urls:
-            images.append(scraper.get(image_url).content, timeout=TIMEOUT)
+            images.append(scraper.get(image_url, timeout=TIMEOUT).content)
         desc = ad_json['advert']['description']
-        deriv = ad_json['vehicle']['derivativeId']
-        specs = scraper.get(tech_specs, params={'derivative':deriv, 'channel':'cars'}).json()
+        vehicle_data = ad_json['vehicle']
+        try:
+            deriv = ad_json['vehicle']['derivativeId']
+            specs = scraper.get(SPECS_ENDPOINT, params={'derivative':deriv, 'channel':'cars'}, timeout=TIMEOUT).json()
+        except KeyError:
+            specs = {'Missing_deriv':True}
 
         # Store relevant data in RAM 
-        data.append({'price':price, 'price_float': price_float, 'description':desc, 'images':images, 'specs':specs})
+        data.append({'price':price, 'price_float': price_float, 'description':desc, 'images':images, 'vehicle': vehicle_data, 'specs':specs})
+        ad_idx += 1
 
     if (page - INITIAL_PAGE) % BACKUP_FREQ == 0 and page > BACKUP_FREQ:
+        print("Backup")
         logging.debug(f'Backing up progress from page {backup_page} to {page}')
-        with open('backup_{backup_page}_{page}', 'wb') as f, open('page_reached.tmp', 'w') as g:
-            pkl.dump(data, f)
+        with open(f'data/backup_{backup_page}_{page}.pickle', 'wb') as f:
+            pkl.dump(data, f, protocol=pkl.HIGHEST_PROTOCOL)
             data = []
             backup_page = page
+
+        with open('page_reached.tmp', 'w') as g:
             g.write(f'{page}')
 
 
     page += 1
-    logging.debug(f'Scraped page - sleeping')
-    sleep(max(delay_s,1))
+    logging.debug('Scraped page - sleeping')
+    sleep(max(2*delay_s,DELAY))
     logging.debug(f'Moving on to scrape page {page}')
     params = {'postcode':'eh42ar', 'page': page, 'sort':SORT_KEY}
     
-    search_page = scraper.get(search, params=params, )
+    search_page = scraper.get(SEARCH_ENDPOINT, params=params, timeout=TIMEOUT)
+    new_delay = search_page.elapsed.total_seconds()
+    if new_delay > 5*delay_s:
+        logging.warning(f"Appear to be barraging server, delay went from {delay_s} to {new_delay}, taking a 10s break.")
+        sleep(10)
+    delay_s = new_delay
     soup = bs(search_page.content, 'html.parser')
+
